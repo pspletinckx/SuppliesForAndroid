a7406795d5e5649bfafcdc1c8051ed4d0aaacf3d
Pieter Pletinckx <pieplay@gmail.com>
2024-08-18 22:08:52 +0200

    2 new Categories

--- app/src/main/java/be/pieterpletinckx/supplystorage/data/Datasource.kt
+++ app/src/main/java/be/pieterpletinckx/supplystorage/data/Datasource.kt
@@ -20,6 +20,8 @@ class Datasource() {
             Category("Food", R.drawable.category_food_canned),
             Category("Cleaning", R.drawable.category_cleaning_spray_soap),
             Category("Bath", R.drawable.category_bath_zen),
+            Category("Cooking",R.drawable.category_cooking_pans),
+            Category("Electronics", R.drawable.category_electronics_pc),
         )
     }
 }
